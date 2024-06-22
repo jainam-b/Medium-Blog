1ea54d31398f6bdd328f8cb7cec3ff0b660f69b7
jainam-b <jainambagrecha16@gmail.com>
2024-06-22 19:53:31 +0530

    add signin and signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,98 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+import { sign, verify } from "hono/jwt";
+import bcrypt from "bcryptjs";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
-app.post(" /api/v1/user/signup", (c) => {
+
+// MIDDLEWARE 
+app.use("/api/v1/blog/*",async(c,next)=>{
+  const header=c.req.header("authorization") || "";
+  const token = header.split(" ")[1]
+  const response = await verify(token,c.env.JWT_SECRET);
+  if(response.id){
+    next()
+  }else{
+    c.status(403);
+    c.json({
+      error:"Unauthorized access"
+    })
+  }
+
+})
+
+app.post("/api/v1/user/signup", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
-  return c.text("Hello Hono!");
+
+  const body = await c.req.json();
+  const hashedPassword = await bcrypt.hash(body.password, 10);
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: hashedPassword,
+      },
+    });
+
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+
+    return c.json({
+      jwt: token,
+    });
+  } catch (error) {
+    return c.json(
+      {
+        error: "Failed to create user.",
+      },
+      500
+    );
+  }
 });
 
-app.post(" /api/v1/user/signup", (c) => {
+app.post(" /api/v1/user/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    if (!user) {
+      c.status(403);
+      return c.json({
+        error: "User not found "
+      })};
+
+      const token= await sign({id: user.id}, c.env.JWT_SECRET)
+      return c.json({token});
+
+
+
+  } catch (error) {
+    return c.json(
+      {
+        error: "Failed to signin",
+      },
+      500
+    );
+  }
+
   return c.text("Hello Hono!");
 });
 
