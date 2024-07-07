d6fc580910452442e15bc3c31c5506477bfb3fbb
jainam-b <jainambagrecha16@gmail.com>
2024-07-07 17:27:22 +0530

    default route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate  } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Blog from "./pages/Blog";
@@ -10,7 +10,8 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<Navigate to="/signup" />} />
+          <Route path="/signup" element={<Signup />}  />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blogs" element={<Blogs/>} />
           <Route path="/blog/:id" element={<Blog />} />
